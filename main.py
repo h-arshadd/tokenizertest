c7699b548e7df14409a7d413078ce086221db0bb
Huda Arshad <hudaarshad2004@gmail.com>
2026-09-19 13:12:37 +0500

    Implement FastAPI application with token generation from user input

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, computed_field
+import tiktoken
+
+app = FastAPI()
+
+class InputFromUser(BaseModel):
+    user_input: str
+
+    @computed_field
+    @property
+    def token_generator(self):
+        tokenizer = tiktoken.encoding_for_model("gpt-4")
+        token_id = tokenizer.encode(self.user_input)
+        return token_id
+
+@app.post("/input")
+async def input_data(data: InputFromUser):
+    return {
+        'message': 'Text received.',
+        'tokens': data.token_generator
+    }
+
